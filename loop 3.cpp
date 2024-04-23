207ac6325d061e9eb22aa14ec5ee8b606a4178b1
MdAjharulIslam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-04-24 00:19:29 +0600

    Add files via upload

---
+++ loop 3.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[2][3];
+
+    cout << "Enter the values of the matrix:" << endl;
+
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << "Enter the value at position (" << i << "," << j << "): ";
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Matrix values:" << endl;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << a[i][j] << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
