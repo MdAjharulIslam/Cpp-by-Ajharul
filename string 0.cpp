82eb3b936cead42222dcfb8d9b4d63d9c06a986c
MdAjharulIslam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-04-27 00:18:03 +0600

    Add files via upload

---
+++ string 0.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main(){
+
+char a[20];
+char b[50];
+printf("enter a string a : ");
+gets(a);
+puts(a);
+printf("enter a string b : ");
+gets(b);
+puts(b);
+
+
+
+return 0;
+}
