a1b0864e186ae3905558768f074fd0c67b033762
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-05-16 01:26:46 +0600

    Add files via upload

---
+++ Basic (2).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+for (int i=0;i<6;i++){
+    for(int j=0;j<i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+for (int i=0;i<6;i++){
+    for(int j=i;j<6;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+
+
+return 0;
+
+}
